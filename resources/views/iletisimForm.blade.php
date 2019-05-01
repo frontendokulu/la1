c81a716482abc0e35c21b2a5986f76c2e84e2261
frontendokulu <frontendokulu@gmail.com>
2019-05-02 01:23:30 +0300

    Route işlemleri ile ilgili çalışma yapıldı, ayrıntılar notlar dosyasında

---
+++ resources/views/iletisimForm.blade.php
@@ -0,0 +1,1 @@
+<h1>İletişim Form Sayfası</h1>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('test','TestController@test');
+//Route::get('iletisim','IletisimController@index');
+Route::resource('iletisim','IletisimController');
